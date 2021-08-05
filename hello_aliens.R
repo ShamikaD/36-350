ebe8ee098030e5237e9fc3d470aadbe1d10b86d6
ShamikaD <dhurishamika@gmail.com>
2021-08-05 13:08:09 -0400

    alien thing

---
+++ hello_aliens.R
@@ -0,0 +1,10 @@
+cat("Hello Aliens. This is a message from Earth. If you can understand this, 
+here are a few questions we have for you:
+    - What planet are you on?
+    - How dod you recive this message?
+    - How did you learn to understand english?
+    - Why are you looking at my homework?
+    
+For your own safety, I reccomend not visiting due to a pandemic named \"COVID-19\"
+that has been causing millions of deaths around the world. If there's any alien 
+supercure that you could give us, that would be great.")
